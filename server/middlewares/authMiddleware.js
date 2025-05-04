9cc19b9fb6f7b1d33e82684128d9b79877a800f8
thuan1952001 <thuanalbion001@gmail.com>
2025-05-04 22:30:08 +0700

    demo2

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+    
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = decoded.userId;
+        req.userId = decoded.userId;
         next();
     } catch (error) {
         res.status(401).send({
             success: false,
-            message: 'Unauthorized access'
+            message: error.message
         });
     }
 }
